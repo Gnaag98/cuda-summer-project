f6cbe9efb1eab1533df9ff2af5916a6e113eb89b
Henrik Wahlström <henrik.g.wahlstrom@gmail.com>
2024-06-17 12:45:49 +0200

    Make constants global
    The constants in main are now global so that they can be accessed by kernels.

--- main.cu
+++ main.cu
@@ -3,33 +3,33 @@
 #include <random>
 #include <vector>
 
+// Around 2 million particles.
+const auto N = 1 << 21;
+
+// Size of 2D space.
+const auto width = 2000.0f;
+const auto height = 4000.0f;
+const auto half_width = width / 2;
+const auto half_height = height / 2;
+
+// Size of 2D density grid.
+const auto U = static_cast<int>(width / 20);
+const auto V = static_cast<int>(height / 20);
+
+const auto random_seed = 1u;
+
+// Allocation size for 1D arrays.
+const auto position_size = N * sizeof(float);
+const auto density_size = U * V * sizeof(float);
+
 __global__
 void print_point(float *x, float *y)
 {
   int i = blockIdx.x * blockDim.x + threadIdx.x;
-  printf("%9.3f, %9.3f\n", x[i], y[i]);
+  printf("(%9.3f, %9.3f)\n", x[i], y[i]);
 }
 
 int main() {
-    // Around 2 million particles.
-    const auto N = 1 << 21;
-
-    // Size of 2D space.
-    const auto width = 2000.0f;
-    const auto height = 4000.0f;
-    const auto half_width = width / 2;
-    const auto half_height = height / 2;
-
-    // Size of 2D density grid.
-    const auto U = static_cast<int>(width / 20);
-    const auto V = static_cast<int>(height / 20);
-
-    const auto random_seed = 1u;
-
-    // Allocation size for 1D arrays.
-    const auto position_size = N * sizeof(float);
-    const auto density_size = U * V * sizeof(float);
-
     // Allocate particle positions and densities on the host.
     auto h_pos_x = std::vector<float>(position_size);
     auto h_pos_y = std::vector<float>(position_size);
@@ -66,6 +66,10 @@ int main() {
 
     // DEBUG: Print 10 device points.
     print_point<<<1, 10>>>(d_pos_x, d_pos_y);
+    cudaDeviceSynchronize();
+    std::cout << '\n';
+
+    print_cell_origin<<<1, 1>>>(d_pos_x, d_pos_y, d_density);
 
     // Free device memory.
     cudaFree(d_pos_x);
